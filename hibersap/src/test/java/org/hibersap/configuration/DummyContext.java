c09ec85c0a23155371627a3ecabda2ed2a09b124
Carsten Erker <carsten.erker@akquinet.de>
2008-09-30 16:29:34 +0000

    new tests

---
+++ hibersap/src/test/java/org/hibersap/configuration/DummyContext.java
@@ -0,0 +1,29 @@
+package org.hibersap.configuration;
+
+import java.util.Properties;
+
+import org.hibersap.HibersapException;
+import org.hibersap.execution.Connection;
+import org.hibersap.session.Context;
+
+public class DummyContext
+    implements Context
+{
+
+    public void configure( Properties properties )
+        throws HibersapException
+    {
+        // do nothing
+    }
+
+    public Connection getConnection()
+    {
+        return null;
+    }
+
+    public void reset()
+    {
+        // do nothing
+    }
+
+}
