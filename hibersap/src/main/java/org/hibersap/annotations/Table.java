add4e2b2559590519298569b9c8639f4e5281411
Carsten Erker <carsten.erker@akquinet.de>
2008-11-14 14:31:03 +0000

    added comment

--- hibersap/src/main/java/org/hibersap/annotations/Table.java
+++ hibersap/src/main/java/org/hibersap/annotations/Table.java
@@ -4,19 +4,17 @@ package org.hibersap.annotations;
  * Copyright (C) 2008 akquinet tech@spree GmbH
  * 
  * This file is part of Hibersap.
- *
- * Hibersap is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * Hibersap is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
  * 
- * You should have received a copy of the GNU Lesser General Public License
- * along with Hibersap.  If not, see <http://www.gnu.org/licenses/>.
+ * Hibersap is free software: you can redistribute it and/or modify it under the terms of the GNU
+ * Lesser General Public License as published by the Free Software Foundation, either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * Hibersap is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License along with Hibersap. If
+ * not, see <http://www.gnu.org/licenses/>.
  */
 
 /**
@@ -31,10 +29,12 @@ import java.lang.annotation.Target;
 
 /**
  * Declare parameter as an table parameter of the Bapi.
- * 
+ *
  * @author Carsten Erker
  */
 @Retention(RUNTIME)
 @Target(value = FIELD)
-public @interface Table {
+public @interface Table
+{
+    // marker annotation
 }
