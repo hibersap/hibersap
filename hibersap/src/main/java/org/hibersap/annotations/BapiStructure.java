4c0f0c2cfd4c40692284040c014aeba8a703b063
Carsten Erker <carsten.erker@akquinet.de>
2008-11-14 14:29:45 +0000

    added comment

--- hibersap/src/main/java/org/hibersap/annotations/BapiStructure.java
+++ hibersap/src/main/java/org/hibersap/annotations/BapiStructure.java
@@ -2,8 +2,10 @@ package org.hibersap.annotations;
 
 /**
  * Declare Bapi return structure
- * 
+ *
  * @author Carsten Erker
  */
-public @interface BapiStructure {
+public @interface BapiStructure
+{
+    // marker annotation
 }
