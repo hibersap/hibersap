5c1b07afa8a936b9604b9d7f7889d6c7935e805e
Markus Dahm <markus.dahm@akquinet.de>
2008-12-12 10:37:23 +0000

    null-safe + more verbose

--- hibersap/src/main/java/org/hibersap/session/SapErrorInterceptor.java
+++ hibersap/src/main/java/org/hibersap/session/SapErrorInterceptor.java
@@ -38,7 +38,7 @@ public class SapErrorInterceptor
     implements ExecutionInterceptor
 {
     // TODO test with return table
-    public void afterExecute( BapiMapping bapiMapping, Map<String, Object> functionMap )
+    public void afterExecute( final BapiMapping bapiMapping, final Map<String, Object> functionMap )
         throws SapException
     {
         if ( bapiMapping.getErrorHandling().isThrowExceptionOnError() )
@@ -47,30 +47,30 @@ public class SapErrorInterceptor
         }
     }
 
-    public void beforeExecute( BapiMapping bapiMapping, Map<String, Object> functionMap )
+    public void beforeExecute( final BapiMapping bapiMapping, final Map<String, Object> functionMap )
     {
         // nothing to do
     }
 
-    private void checkForErrors( BapiMapping bapiMapping, Map<String, Object> functionMap )
+    private void checkForErrors( final BapiMapping bapiMapping, final Map<String, Object> functionMap )
         throws SapException
     {
-        ErrorHandling errorHandling = bapiMapping.getErrorHandling();
+        final ErrorHandling errorHandling = bapiMapping.getErrorHandling();
 
-        String[] path = errorHandling.getPathToReturnStructure().split( "/" );
-        Map<String, Object> containingMap = getContainingMap( bapiMapping, functionMap, path );
+        final String[] path = errorHandling.getPathToReturnStructure().split( "/" );
+        final Map<String, Object> containingMap = getContainingMap( bapiMapping, functionMap, path );
 
-        String nameOfReturnStructure = path[path.length - 1];
-        Object returnObj = containingMap.get( nameOfReturnStructure );
+        final String nameOfReturnStructure = path[path.length - 1];
+        final Object returnObj = containingMap.get( nameOfReturnStructure );
         Collection<Map<String, Object>> lines;
 
         if ( returnObj instanceof Map )
         {
             // we got a return structure
-            Map<String, Object> returnMap = UnsafeCastHelper.castToMap( returnObj );
+            final Map<String, Object> returnMap = UnsafeCastHelper.castToMap( returnObj );
             lines = Collections.singletonList( returnMap );
         }
-        else if ( Collection.class.isAssignableFrom( returnObj.getClass() ) )
+        else if ( returnObj instanceof Collection )
         {
             // we got a return table
             lines = UnsafeCastHelper.castToCollectionOfMaps( returnObj );
@@ -79,20 +79,21 @@ public class SapErrorInterceptor
         {
             throw new MappingException( "Checking for errors failed: Parameter returnStructure of Annotation "
                 + ThrowExceptionOnError.class.getSimpleName() + " in Class " + bapiMapping.getAssociatedClass()
-                + " does not point to a structure or table of function module " + bapiMapping.getBapiName() );
+                + " does not point to a structure or table of function module " + bapiMapping.getBapiName() + "."
+                + nameOfReturnStructure + " = " + returnObj );
         }
 
-        String[] messageTypes = errorHandling.getErrorMessageTypes();
+        final String[] messageTypes = errorHandling.getErrorMessageTypes();
         checkSapErrors( messageTypes, lines );
     }
 
-    private Map<String, Object> getContainingMap( BapiMapping bapiMapping, Map<String, Object> functionMap,
-                                                  String[] path )
+    private Map<String, Object> getContainingMap( final BapiMapping bapiMapping, final Map<String, Object> functionMap,
+                                                  final String[] path )
     {
         Map<String, Object> containingMap = functionMap;
         for ( int i = 0; i < path.length - 1; i++ )
         {
-            Object containingObj = containingMap.get( path[i].trim() );
+            final Object containingObj = containingMap.get( path[i].trim() );
             if ( containingObj instanceof Map )
             {
                 containingMap = UnsafeCastHelper.castToMap( containingObj );
@@ -101,8 +102,8 @@ public class SapErrorInterceptor
             {
                 throw new MappingException( "Checking for errors failed: Path element " + path[i]
                     + " does not point to a valid structure type of function module " + bapiMapping.getBapiName()
-                    + ". See parameter returnStructure of Annotation " + ThrowExceptionOnError.class.getSimpleName()
-                    + " in Class " + bapiMapping.getAssociatedClass() );
+                    + ", but is a " + containingObj + ". See parameter returnStructure of Annotation "
+                    + ThrowExceptionOnError.class.getSimpleName() + " in Class " + bapiMapping.getAssociatedClass() );
             }
         }
         if ( containingMap == null )
@@ -114,20 +115,20 @@ public class SapErrorInterceptor
         return containingMap;
     }
 
-    private void checkSapErrors( String[] messageTypes, Collection<Map<String, Object>> returnTable )
+    private void checkSapErrors( final String[] messageTypes, final Collection<Map<String, Object>> returnTable )
         throws SapException
     {
-        ArrayList<SapError> sapErrors = new ArrayList<SapError>();
-        for ( Map<String, Object> map : returnTable )
+        final ArrayList<SapError> sapErrors = new ArrayList<SapError>();
+        for ( final Map<String, Object> map : returnTable )
         {
-            String type = (String) map.get( "TYPE" );
+            final String type = (String) map.get( "TYPE" );
             if ( ArrayUtils.contains( messageTypes, type ) )
             {
                 // TODO make casts safe / error tolerant
-                String id = (String) map.get( "ID" );
-                String number = (String) map.get( "NUMBER" );
-                String message = (String) map.get( "MESSAGE" );
-                SapError sapError = new SapError( type, id, number, message );
+                final String id = (String) map.get( "ID" );
+                final String number = (String) map.get( "NUMBER" );
+                final String message = (String) map.get( "MESSAGE" );
+                final SapError sapError = new SapError( type, id, number, message );
                 sapErrors.add( sapError );
             }
         }
