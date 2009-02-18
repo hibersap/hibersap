1738aebba08520e1eef89381f71334efbe0365aa
Torsten Fink <torsten.fink@akquinet.de>
2009-02-18 10:42:00 +0000

    fixed a bug in the SessionManagerConfig

--- hibersap-project/hibersap/hibersap-core/src/main/java/org/hibersap/configuration/xml/SessionManagerConfig.java
+++ hibersap-project/hibersap/hibersap-core/src/main/java/org/hibersap/configuration/xml/SessionManagerConfig.java
@@ -1,6 +1,8 @@
 package org.hibersap.configuration.xml;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
 
 import javax.xml.bind.annotation.XmlAccessType;
@@ -11,124 +13,156 @@ import javax.xml.bind.annotation.XmlElementWrapper;
 import javax.xml.bind.annotation.XmlTransient;
 import javax.xml.bind.annotation.XmlType;
 
-@XmlType(namespace = HibersapConfig.NAMESPACE, propOrder = { "context", "jcaConnectionFactory", "properties", "classes" })
+@XmlType(namespace = HibersapConfig.NAMESPACE, propOrder = { "context",
+		"jcaConnectionFactory", "properties", "classes" })
 @XmlAccessorType(XmlAccessType.PROPERTY)
-public class SessionManagerConfig
-{
+public class SessionManagerConfig {
 
-    @XmlTransient
-    private static final long serialVersionUID = 1;
+	@XmlTransient
+	private static final long serialVersionUID = 1;
 
-    private String name;
+	private String name;
 
-    private String context;
+	private String context;
 
-    private Set<Property> properties = new HashSet<Property>( 0 );
+	private final Set<Property> properties = new HashSet<Property>();
 
-    private Set<String> classes = new HashSet<String>( 0 );
+	private Map<String, String> nameValues = null;
 
-    private String jcaConnectionFactory;
+	private Set<String> classes = new HashSet<String>();
 
-    public SessionManagerConfig()
-    {
-    }
+	private String jcaConnectionFactory;
 
-    public SessionManagerConfig( String name )
-    {
-        this.name = name;
-    }
+	public SessionManagerConfig() {
+		System.out.println("PUB-CONSTRUCTOR");
+		System.out.println("properties = " + properties);
+	}
 
-    SessionManagerConfig( final String name, final String context, final Set<Property> properties )
-    {
-        super();
-        this.name = name;
-        this.context = context;
-        this.properties = properties;
-    }
+	public SessionManagerConfig(final String name) {
+		this.name = name;
+		System.out.println("CONSTRUCTOR #1");
+		System.out.println("properties = " + properties + " name=" + name);
+	}
 
-    @XmlAttribute(required = true)
-    public String getName()
-    {
-        return name;
-    }
+	SessionManagerConfig(final String name, final String context,
+			final Set<Property> properties) {
+		super();
+		System.out.println("CONSTRUCTOR #2");
+		System.out.println("properties = " + properties + " name=" + name);
+		this.name = name;
+		this.context = context;
+		setProperties(properties);
+	}
 
-    public SessionManagerConfig setName( final String name )
-    {
-        this.name = name;
-        return this;
-    }
+	@XmlAttribute(required = true)
+	public String getName() {
+		return name;
+	}
 
-    @XmlElement(name = "context", required = false, namespace = HibersapConfig.NAMESPACE)
-    public String getContext()
-    {
-        return context;
-    }
+	public SessionManagerConfig setName(final String name) {
+		this.name = name;
+		return this;
+	}
 
-    @XmlElement(name = "jca-connection-factory", required = false, namespace = HibersapConfig.NAMESPACE)
-    public String getJcaConnectionFactory()
-    {
-        return jcaConnectionFactory;
-    }
+	@XmlElement(name = "context", required = false, namespace = HibersapConfig.NAMESPACE)
+	public String getContext() {
+		return context;
+	}
 
-    @XmlElement(name = "property", namespace = HibersapConfig.NAMESPACE)
-    @XmlElementWrapper(name = "properties", namespace = HibersapConfig.NAMESPACE)
-    public Set<Property> getProperties()
-    {
-        return properties;
-    }
+	@XmlElement(name = "jca-connection-factory", required = false, namespace = HibersapConfig.NAMESPACE)
+	public String getJcaConnectionFactory() {
+		return jcaConnectionFactory;
+	}
 
-    public SessionManagerConfig setJcaConnectionFactory( final String jcaConnectionFactory )
-    {
-        this.jcaConnectionFactory = jcaConnectionFactory;
-        return this;
-    }
+	@XmlElement(name = "property", namespace = HibersapConfig.NAMESPACE)
+	@XmlElementWrapper(name = "properties", namespace = HibersapConfig.NAMESPACE)
+	public Set<Property> getProperties() {
+		return properties;
+	}
 
-    public void setProperties( final Set<Property> properties )
-    {
-        this.properties = properties;
-    }
+	public SessionManagerConfig setJcaConnectionFactory(
+			final String jcaConnectionFactory) {
+		this.jcaConnectionFactory = jcaConnectionFactory;
+		return this;
+	}
 
-    @XmlElement(name = "class", namespace = HibersapConfig.NAMESPACE)
-    @XmlElementWrapper(name = "annotated-classes", namespace = HibersapConfig.NAMESPACE)
-    public Set<String> getClasses()
-    {
-        return classes;
-    }
+	public void setProperties(final Set<Property> properties) {
+		System.out.println("SETPROPS");
+		this.properties.clear();
+		this.properties.addAll(properties);
+		nameValues = null;
 
-    public void setClasses( final Set<String> classes )
-    {
-        this.classes = classes;
-    }
+	}
 
-    public String getProperty( String name )
-    {
-        for ( Property property : properties )
-        {
-            if ( property.getName().equals( name ) )
-                return property.getValue();
-        }
-        return null;
-    }
+	@XmlElement(name = "class", namespace = HibersapConfig.NAMESPACE)
+	@XmlElementWrapper(name = "annotated-classes", namespace = HibersapConfig.NAMESPACE)
+	public Set<String> getClasses() {
+		return classes;
+	}
 
-    public SessionManagerConfig setContext( String context )
-    {
-        this.context = context;
-        return this;
-    }
+	public void setClasses(final Set<String> classes) {
+		this.classes = classes;
+	}
 
-    public SessionManagerConfig setProperty( String name, String value )
-    {
-        Property property = new Property( name, value );
-        if ( properties.contains( property ) )
-        {
-        }
-        properties.add( property );
-        return this;
-    }
+	public String getProperty(final String name) {
+		return getNameValues().get(name);
+	}
 
-    public SessionManagerConfig addClass( Class<?> annotatedClass )
-    {
-        classes.add( annotatedClass.getName() );
-        return this;
-    }
+	public SessionManagerConfig setContext(final String context) {
+		this.context = context;
+		return this;
+	}
+
+	public SessionManagerConfig setProperty(final String name,
+			final String value) {
+		final String currentValue = getNameValues().get(name);
+		if (currentValue != null) {
+			final Property oldProperty = new Property(name, currentValue);
+			assert properties.contains(oldProperty);
+			final boolean oldValueExisted = properties.remove(oldProperty);
+			assert oldValueExisted;
+		}
+
+		// do not use the getter, because the two collections are temporarily
+		// out of sync
+		// TODO: think about errors
+		nameValues.put(name, value);
+		final Property newProperty = new Property(name, value);
+		properties.add(newProperty);
+		assert nameValues.size() == properties.size() : nameValues.size()
+				+ " != " + properties.size();
+
+		return this;
+	}
+
+	public SessionManagerConfig addClass(final Class<?> annotatedClass) {
+		classes.add(annotatedClass.getName());
+		return this;
+	}
+
+	private Map<String, String> getNameValues() {
+		// This is pretty complicated and could be simplified, if the
+		// JAXB implementation is actually
+		// @XmlAccessorType(XmlAccessType.PROPERTY)
+		// Unfortunately the value of properties is still set to the field,
+		// thus, we have to build the map lazily...
+		if (nameValues == null) {
+			nameValues = new HashMap<String, String>(properties.size());
+			for (final Property property : properties) {
+				final String oldValue = nameValues.put(property.getName(),
+						property.getValue());
+			}
+		}
+		assert nameValues != null;
+		assert nameValues.size() == properties.size() : "Map "
+				+ nameValues.size() + "!= Set " + properties.size();
+
+		return nameValues;
+	}
+
+	@Override
+	public String toString() {
+		return "Session Configuration: " + name + "\nContext: " + context
+				+ "\nProperties: " + properties + "\nClasses: " + classes;
+	}
 }
--- hibersap-project/hibersap/hibersap-core/src/test/java/org/hibersap/configuration/ConfigurationTest.java
+++ hibersap-project/hibersap/hibersap-core/src/test/java/org/hibersap/configuration/ConfigurationTest.java
@@ -34,83 +34,81 @@ import org.junit.Test;
 /**
  * @author Carsten Erker
  */
-public class ConfigurationTest
-{
-    private Configuration configuration;
+public class ConfigurationTest {
+	private static final int NUMBER_OF_PROPERTIES_IN_TEST_CONFIGURATION = 7;
+	private Configuration configuration;
 
-    @Before
-    @SuppressWarnings("serial")
-    public void createConfiguration()
-    {
-        configuration = new Configuration()
-        {
-            // nothing to overwrite
-        };
-        configuration.getSessionManagerConfig().setContext( DummyContext.class.getName() );
-    }
+	@Before
+	@SuppressWarnings("serial")
+	public void createConfiguration() {
+		configuration = new Configuration() {
+			// nothing to overwrite
+		};
+		configuration.getSessionManagerConfig().setContext(
+				DummyContext.class.getName());
+	}
 
-    @Test
-    public void addInterceptor()
-    {
-        ExecutionInterceptor dummyInterceptor = new ExecutionInterceptor()
-        {
-            public void afterExecute( BapiMapping bapiMapping, Map<String, Object> functionMap )
-            {
-                // dummy
-            }
+	@Test
+	public void addInterceptor() {
+		final ExecutionInterceptor dummyInterceptor = new ExecutionInterceptor() {
+			public void afterExecute(BapiMapping bapiMapping,
+					Map<String, Object> functionMap) {
+				// dummy
+			}
 
-            public void beforeExecute( BapiMapping bapiMapping, Map<String, Object> functionMap )
-            {
-                // dummy
-            }
-        };
-        configuration.addInterceptor( dummyInterceptor );
-        SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration.buildSessionManager();
+			public void beforeExecute(BapiMapping bapiMapping,
+					Map<String, Object> functionMap) {
+				// dummy
+			}
+		};
+		configuration.addInterceptor(dummyInterceptor);
+		final SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration
+				.buildSessionManager();
 
-        assertTrue( sessionManager.getInterceptors().contains( dummyInterceptor ) );
-    }
+		assertTrue(sessionManager.getInterceptors().contains(dummyInterceptor));
+	}
 
-    @Test
-    public void setGetOverwriteProperties()
-        throws Exception
-    {
-        SessionManagerConfig sfConfig = configuration.getSessionManagerConfig();
+	@Test
+	public void setGetOverwriteProperties() throws Exception {
+		SessionManagerConfig sfConfig = configuration.getSessionManagerConfig();
+		// overwrites context class
+		assertEquals(DummyContext.class.getName(), sfConfig.getContext());
+		sfConfig.setContext("test");
+		assertEquals("test", sfConfig.getContext());
 
-        // overwrites context class
-        assertEquals( DummyContext.class.getName(), sfConfig.getContext() );
-        sfConfig.setContext( "test" );
-        assertEquals( "test", sfConfig.getContext() );
+		// overwrites property
+		assertEquals("user", sfConfig.getProperty("jco.client.user"));
+		sfConfig.setProperty("jco.client.user", "test");
+		assertEquals("test", sfConfig.getProperty("jco.client.user"));
 
-        // overwrites property
-        assertEquals( "user", sfConfig.getProperty( "jco.client.user" ) );
-        sfConfig.setProperty( "jco.client.user", "test" );
-        assertEquals( "test", sfConfig.getProperty( "jco.client.user" ) );
+		// overwrites whole configuration
+		assertEquals(NUMBER_OF_PROPERTIES_IN_TEST_CONFIGURATION, sfConfig
+				.getProperties().size());
+		final SessionManagerConfig config = new SessionManagerConfig()
+				.setProperty("testkey", "testvalue");
+		configuration.setSessionManagerConfig(config);
+		sfConfig = configuration.getSessionManagerConfig();
+		assertEquals(1, sfConfig.getProperties().size());
+		assertEquals("testvalue", sfConfig.getProperty("testkey"));
+	}
 
-        // overwrites whole configuration
-        assertEquals( 8, sfConfig.getProperties().size() );
-        SessionManagerConfig config = new SessionManagerConfig().setProperty( "testkey", "testvalue" );
-        configuration.setSessionManagerConfig( config );
-        sfConfig = configuration.getSessionManagerConfig();
-        assertEquals( 1, sfConfig.getProperties().size() );
-        assertEquals( "testvalue", sfConfig.getProperty( "testkey" ) );
-    }
+	@Test
+	public void settingsInitialized() {
+		final SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration
+				.buildSessionManager();
+		final Settings settings = sessionManager.getSettings();
 
-    @Test
-    public void settingsInitialized()
-    {
-        SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration.buildSessionManager();
-        Settings settings = sessionManager.getSettings();
+		// Context class
+		assertEquals(DummyContext.class, settings.getContext().getClass());
+	}
 
-        // Context class
-        assertEquals( DummyContext.class, settings.getContext().getClass() );
-    }
-
-    @Test
-    public void standardInterceptorsInitialized()
-    {
-        SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration.buildSessionManager();
-        List<ExecutionInterceptor> interceptors = sessionManager.getInterceptors();
-        assertEquals( 1, interceptors.size() );
-        assertEquals( SapErrorInterceptor.class, interceptors.get( 0 ).getClass() );
-    }
+	@Test
+	public void standardInterceptorsInitialized() {
+		final SessionManagerImplementor sessionManager = (SessionManagerImplementor) configuration
+				.buildSessionManager();
+		final List<ExecutionInterceptor> interceptors = sessionManager
+				.getInterceptors();
+		assertEquals(1, interceptors.size());
+		assertEquals(SapErrorInterceptor.class, interceptors.get(0).getClass());
+	}
 }
