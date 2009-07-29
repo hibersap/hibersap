a0b3c8a3e22694bddf563091cb917fa9352cbca0
Carsten Erker <carsten.erker@akquinet.de>
2009-07-29 17:42:49 +0000

    moved dependency to sap-jco from hibersap-core to hibersap-jco

--- hibersap-project/hibersap/hibersap-jco/src/main/java/org/hibersap/execution/jco/JCoContext.java
+++ hibersap-project/hibersap/hibersap-jco/src/main/java/org/hibersap/execution/jco/JCoContext.java
@@ -89,4 +89,33 @@ public class JCoContext
     {
         return new JCoConnection( destinationName );
     }
+
+    @Override
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ( ( destinationName == null ) ? 0 : destinationName.hashCode() );
+        return result;
+    }
+
+    @Override
+    public boolean equals( Object obj )
+    {
+        if ( this == obj )
+            return true;
+        if ( obj == null )
+            return false;
+        if ( getClass() != obj.getClass() )
+            return false;
+        JCoContext other = (JCoContext) obj;
+        if ( destinationName == null )
+        {
+            if ( other.destinationName != null )
+                return false;
+        }
+        else if ( !destinationName.equals( other.destinationName ) )
+            return false;
+        return true;
+    }
 }
