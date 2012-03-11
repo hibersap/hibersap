e779fd7a03ffe35602942c160480a3419a0366af
Carsten Erker <carsten.erker@akquinet.de>
2012-03-11 19:43:31 +0000

    organize imports

--- hibersap/hibersap-core/src/main/java/org/hibersap/configuration/Environment.java
+++ hibersap/hibersap-core/src/main/java/org/hibersap/configuration/Environment.java
@@ -17,7 +17,6 @@ package org.hibersap.configuration;
  * not, see <http://www.gnu.org/licenses/>.
  */
 
-import com.sun.tools.javac.resources.version;
 import org.hibersap.HibersapException;
 
 import java.io.IOException;
