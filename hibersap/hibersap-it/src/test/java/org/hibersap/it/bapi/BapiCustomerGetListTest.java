6fc9e9736530355f381d34cab25d7ba46591d670
Carsten Erker <carsten.erker@akquinet.de>
2012-09-11 14:10:27 +0000

    add test mapping of table parameter to array

---
+++ hibersap/hibersap-it/src/test/java/org/hibersap/it/bapi/BapiCustomerGetListTest.java
@@ -0,0 +1,42 @@
+package org.hibersap.it.bapi;
+
+import org.hibersap.annotations.Bapi;
+import org.hibersap.annotations.BapiStructure;
+import org.hibersap.annotations.Import;
+import org.hibersap.annotations.Parameter;
+import org.hibersap.annotations.Table;
+import org.hibersap.annotations.ThrowExceptionOnError;
+import org.hibersap.it.AbstractBapiTest;
+import org.junit.Test;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class BapiCustomerGetListTest extends AbstractBapiTest
+{
+    @Test
+    public void mapsTableParameterToArray() throws Exception
+    {
+        BapiCustomerGetList bapi = new BapiCustomerGetList();
+        session.execute( bapi );
+
+        assertThat( bapi.customerList ).hasSize( 2 );
+    }
+
+    @Bapi( "BAPI_FLCUST_GETLIST" )
+    @ThrowExceptionOnError( returnStructure = "TABLE/RETURN" )
+    private static class BapiCustomerGetList
+    {
+        @Import
+        @Parameter( "MAX_ROWS" )
+        int maxRows = 2;
+
+        @Table
+        @Parameter( "CUSTOMER_LIST" )
+        CustomerList[] customerList;
+    }
+
+    @BapiStructure
+    private static class CustomerList
+    {
+    }
+}
